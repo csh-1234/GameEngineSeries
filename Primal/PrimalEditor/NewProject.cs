793ec7cd71fb74e5bb4c0f5d2093496ffd084b9c
csh-1234 <tmdtmdtmd4@naver.com>
2024-10-09 20:36:37 +0900

    002-Loading project templates for game projects
    서브 메뉴 클릭 이벤트 생성과 xml 파일 추가

---
+++ Primal/PrimalEditor/NewProject.cs
@@ -0,0 +1,95 @@
+﻿using PrimalEditor.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimalEditor.GameProject
+{
+    [DataContract]
+    public class ProjectTemplate
+    {
+        [DataMember]
+        public string ProjectType { get; set; }
+        [DataMember]
+        public string ProjectFile { get; set; }
+        [DataMember]
+        public List<string> Folders { get; set; }
+
+        public byte[] Icon { get; set; }
+        public byte[] ScreenShot { get; set; }
+        public string IconFilePath { get; set; }
+        public string ScreenshotFilePath { get; set; }
+        public string ProjectFilePath { get; set; }
+
+    }
+
+    internal class NewProject : ViewModelBase
+    {
+        private readonly string _templatePath = @"..\..\PrimalEditor\ProjectTemplates";
+        private string _projectName = "NewProject";
+        public string ProjectName
+        {
+            get => _projectName;
+            set
+            {
+                if (_projectName != value)
+                {
+                    _projectName = value;
+                    OnPropertyChanged(nameof(ProjectName));
+                }
+            }
+        }
+
+        private string _projectPath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\PrimalProject\";
+        public string ProjectPath
+        {
+            get => _projectPath;
+            set
+            {
+                if (_projectPath != value)
+                {
+                    _projectPath = value;
+                    OnPropertyChanged(nameof(ProjectPath));
+                }
+            }
+        }
+
+        private ObservableCollection<ProjectTemplate> _projectTemplates = new ObservableCollection<ProjectTemplate>();
+        public ReadOnlyObservableCollection<ProjectTemplate> ProjectTemplates
+        {
+            get;
+        }
+
+        public NewProject()
+        {
+            try
+            {
+                ProjectTemplates = new ReadOnlyObservableCollection<ProjectTemplate>(_projectTemplates);
+                var templateFiles = Directory.GetFiles(_templatePath, "template.xml", SearchOption.AllDirectories);
+                Debug.Assert(templateFiles.Any());
+                foreach (var file in templateFiles)
+                {
+                    var template = Serializer.FromFile<ProjectTemplate>(file);
+                    template.IconFilePath = System.IO.Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), "Icon.png"));
+                    template.Icon = File.ReadAllBytes(template.IconFilePath);
+                    template.ScreenshotFilePath = System.IO.Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), "Screenshot.png"));
+                    template.ScreenShot = File.ReadAllBytes(template.ScreenshotFilePath);
+                    template.ProjectFilePath = System.IO.Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), template.ProjectFile));
+
+                    _projectTemplates.Add(template);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                //TODO : log error
+            }
+        }
+    }
+}
